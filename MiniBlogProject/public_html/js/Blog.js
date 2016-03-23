bacd4073ae9166b72bcc27ccac57f6814e8cee6f
halofreek45 <halofreek45@gmail.com>
2016-03-22 22:28:37 -0700

    Video 9 did not work. User and Backendless were undefined

--- MiniBlogProject/public_html/js/Blog.js
+++ MiniBlogProject/public_html/js/Blog.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(function(){
+    var APPLICATION_ID = "2990FB08-878B-34F0-FFD2-41F20EAC4200",
+        SECRET_KEY = "F1D21139-75B1-0776-FF22-7C2F4CF2EB00",
+        VERSION = "v1";
+    
+  Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+  user.email = "halofreek45@gmail.com";
+  user.password = "JUSTDOIT";
+  Backendless.userService.register(user);
+});
 
